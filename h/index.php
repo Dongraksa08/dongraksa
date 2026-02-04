d9c15d3b7597ab5e5f213e1a815087d847da604b
Dongraksa08 <66010914007@msu.ac.th>
2026-02-04 15:25:10 +0700

    gg

--- h/index.php
+++ h/index.php
@@ -13,5 +13,27 @@ Username <input type="text" name="auser" autofocus required><br>
 Password <input type="password" name="pang" required ><br>
 <button type="submit" name="Submit">LOGIN</button>
 </form>
+
+<?php
+if(isset($_POST['Submit'])){
+   include_once("connectdb.php");
+   $sql = "SELECT" * FRPM admin WHERE a_username='{$_POST['auser']}' AND a_password='{$_POST['pang']}'LIMIT 1";
+   $rs = mysqli_query($conn,$sql);
+   $num = mysqli_num_rows($rs);
+   
+   if ($num == 1) {
+       $data = mysqli_fetch_array($rs);
+       $_SESSION['aid'] = $data['a_id'] ;
+       $_SESSION['aname'] = $data['a_id'] ;
+       echo "<script>";
+       echo "window.location=index2.php';";
+       echo "</script>" ;
+   } else {
+       echo "<script>" ;
+       echo "alert(Username หรือ Password ไม่ถูกต้อง);";
+       echo "</script>" ;
+   }
+}
+?>
 </body>
 </html>
