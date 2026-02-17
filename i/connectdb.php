a749b25ee00d6861db792b8dc2b218c3117a4bab
Dongraksa08 <66010914007@msu.ac.th>
2026-02-17 19:50:00 +0700

    update a.php layout

---
+++ i/connectdb.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pwd = "r660109"; 
+$db = "4007db";
+
+// เชื่อมต่อฐานข้อมูล
+$conn = mysqli_connect($host, $user, $pwd, $db) or die("เชื่อมต่อฐานข้อมูลไม่ได้");
+
+// ตั้งค่าให้อ่านภาษาไทยได้
+mysqli_query($conn, "SET NAMES utf8");
+?>
