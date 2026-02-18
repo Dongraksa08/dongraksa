e8222ada6aae453f01490782ffbb276b0b2c0afc
Dongraksa08 <66010914007@msu.ac.th>
2026-02-18 16:42:40 +0700

    tt

---
+++ k/a.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+    <p>66010914007 ดวงรักษา อรเพ็ชร</p>
+    
+    <img src="1.jpg" alt="รูปภาพดวงรักษา" width="300">
+</body>
+</html>
